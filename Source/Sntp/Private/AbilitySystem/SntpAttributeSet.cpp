52d9940664043a8d0b9f0f1975f4e0c0630cc225
nowifi233-ui <nowifi233@gmail.com>
2026-03-01 15:39:38 +0800

    add health and mana to Attributes

--- Source/Sntp/Private/AbilitySystem/SntpAttributeSet.cpp
+++ Source/Sntp/Private/AbilitySystem/SntpAttributeSet.cpp
@@ -2,4 +2,40 @@
 
 
 #include "AbilitySystem/SntpAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+USntpAttributeSet::USntpAttributeSet()
+{
+	InitHealth(100.f);
+	InitMana(50.f);
+}
+
+void USntpAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME_CONDITION_NOTIFY(USntpAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(USntpAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(USntpAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(USntpAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void USntpAttributeSet::OnRep_Health(const FGameplayAttributeData& OldValue) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(USntpAttributeSet, Health, OldValue);
+}
+
+void USntpAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldValue) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(USntpAttributeSet, MaxHealth, OldValue);
+}
+
+void USntpAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldValue) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(USntpAttributeSet, Mana, OldValue);
+}
+
+void USntpAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldValue) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(USntpAttributeSet, MaxMana, OldValue);
+}
--- Source/Sntp/Public/AbilitySystem/SntpAttributeSet.h
+++ Source/Sntp/Public/AbilitySystem/SntpAttributeSet.h
@@ -3,15 +3,50 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
 #include "AttributeSet.h"
 #include "SntpAttributeSet.generated.h"
 
 /**
  * 
  */
+
 UCLASS()
 class SNTP_API USntpAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+public:
+	USntpAttributeSet();
 	
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	// Attribute Data
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category="Vital Attributes")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS_BASIC(USntpAttributeSet, Health)
+	
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category="Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS_BASIC(USntpAttributeSet, MaxHealth)
+	
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category="Vital Attributes")
+	FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS_BASIC(USntpAttributeSet, Mana)
+	
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category="Vital Attributes")
+	FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS_BASIC(USntpAttributeSet, MaxMana)
+	
+	// OnReps
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldValue) const;
+	
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldValue) const;
+	
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldValue) const;
+	
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldValue) const;
 };
